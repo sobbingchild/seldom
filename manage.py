c05f2216155d08a3ceb96ad95c725edd687168b1
sobbingchild <66291315+sobbingchild@users.noreply.github.com>
2026-08-10 11:23:10 +0800

    aa

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import django
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -11,7 +12,6 @@ if __name__ == "__main__":
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
